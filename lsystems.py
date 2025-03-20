e9b717a0db1b2ae48dfdc8b020ae012907974de1
lesliekim1 <lskim@loyola.edu>
2025-03-19 22:21:57 -0400

    edit 1 line

--- lsystems.py
+++ lsystems.py
@@ -19,14 +19,18 @@ def main():
         'C': ['B', 'A', 'C', 'C', 'B']
     }
 
+    my_rules3 = {
+        'A': ['C', 'A', 'B', 'C', 'C'],
+        'B': ['A', 'C', 'B', 'A', 'A'],
+        'C': ['B', 'A', 'C', 'C', 'B']
+    }
+
     my_rule_table = [my_rules, my_rules2]
     my_axiom = ['A']
 
     # Generate L-Systems sequence with multiple iterations
     for i in range(7):
         my_axiom = lindemayer_translation(random.choice(my_rule_table), my_axiom)
-
-    print("Final result:", "".join(my_axiom))
     draw_lsystem(my_axiom)
 
 # Applies L-System rules to transform the axiom iteratively.
@@ -47,7 +51,7 @@ def is_out_of_bounds(t):
     x, y = t.xcor(), t.ycor()
     return abs(x) > BOUNDX or abs(y) > BOUNDY
 
-# Creates a new turtle with a specified color, starting position, and direction
+# Create a new turtle with a specified color, starting position, and direction
 def create_turtle(color, start_pos, angle):
     t = turtle.Turtle()
     t.speed(-1)
@@ -69,10 +73,8 @@ def draw_lsystem(axiom, step=10, angle=45):
         create_turtle("green", (0, -100), random.randint(0, 360)),
         create_turtle("blue", (100, 100), random.randint(0, 360))
     ]
-
     color_shift = [0, 50, 100]
     directions = [1, 1, 1]
-
     for symbol in axiom:
         for i, t in enumerate(turtles):
             # Cycle colors
